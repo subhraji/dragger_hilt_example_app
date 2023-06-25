264b1153c781491df5f7c5ab43b5fabe54429f99
subhrajitdeb <subhrajitdeb54@gmail.com>
2023-06-25 22:45:28 +0530

    test - 25/06/2023

---
+++ app/src/main/java/com/example/hiltapp/ui/activity/DataBindingTestActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hiltapp.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hiltapp.R
+
+class DataBindingTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_binding_test)
+    }
+}
