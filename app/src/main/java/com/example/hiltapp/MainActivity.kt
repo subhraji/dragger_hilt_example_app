74255fd6127ffb01a05f7112591c9a9474c4ce35
subhrajit <subhrajit.deb@ekodus.com>
2023-06-26 18:02:51 +0530

    lang change

--- app/src/main/java/com/example/hiltapp/MainActivity.kt
+++ app/src/main/java/com/example/hiltapp/MainActivity.kt
@@ -1,23 +1,25 @@
 package com.example.hiltapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.hiltapp.databinding.ActivityMainBinding
 import com.example.hiltapp.repository.Outcome
 import com.example.hiltapp.viewmodels.GetTodosViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     lateinit var mainViewModel: GetTodosViewModel
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +28,72 @@ class MainActivity : AppCompatActivity() {
 
         mainViewModel = ViewModelProvider(this).get(GetTodosViewModel::class.java)
 
+        loadLocal()
+        binding.changeLangBtn.setOnClickListener {
+            changeLanguage()
+        }
 
         //observe
         //observeApiCall()
     }
 
+    private fun changeLanguage(){
+        // initialise the list items for the alert dialog
+        val listItems = arrayOf("English", "Assamese")
+        val checkedItems = BooleanArray(listItems.size)
+
+        // copy the items from the main list to the selected item list for the preview
+        // if the item is checked then only the item should be displayed for the user
+        val selectedItems = mutableListOf(*listItems)
+
+        // initialise the alert dialog builder
+        val builder = AlertDialog.Builder(this)
+
+        // set the title for the alert dialog
+        builder.setTitle("Choose Language")
+
+        // set the icon for the alert dialog
+        builder.setIcon(R.drawable.ic_launcher_foreground)
+
+        // now this is the function which sets the alert dialog for multiple item selection ready
+        builder.setMultiChoiceItems(listItems, checkedItems) { dialog, which, isChecked ->
+            checkedItems[which] = isChecked
+            val currentItem = selectedItems[which]
+
+            if(which == 0){
+                setLocal("en")
+                recreate()
+            }else if(which == 1){
+                setLocal("as")
+                recreate()
+            }
+        }
+
+        builder.setCancelable(false)
+        builder.setNegativeButton("CANCEL") { dialog, which -> }
+        builder.create()
+        val alertDialog = builder.create()
+        alertDialog.show()
+    }
+
+    private fun setLocal(language: String){
+        var local: Locale = Locale(language)
+        Locale.setDefault(local)
+        var configuration: Configuration = Configuration()
+        configuration.setLocale(local)
+        baseContext.resources.updateConfiguration(configuration, baseContext.resources.displayMetrics)
+
+        val sharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE)
+        val myEdit = sharedPreferences.edit()
+        myEdit.putString("app_lang", language)
+        myEdit.apply()
+    }
+
+    private fun loadLocal(){
+        val preferences = getSharedPreferences("Settings", MODE_PRIVATE)
+        val language: String = preferences.getString("app_lang","").toString()
+        setLocal(language)
+    }
 
     private fun observeApiCall(){
         mainViewModel.todosLiveData.observe(this, Observer { outcome ->
